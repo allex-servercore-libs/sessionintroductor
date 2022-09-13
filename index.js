5c7ed20075f631f26c6542ca1c543ddf3ed2d115
Andrija Petrovic <andrija.hers@gmail.com>
2022-09-13 15:34:57 +0000

    Self-cleaning introduced

--- index.js
+++ index.js
@@ -1,18 +1,63 @@
 function createIntroductor(lib){
   'use strict';
   var __introducees = new lib.Map();
+
+  var MAXLIFETIME = 10 * lib.intervals.Minute;
+  var __scheduled = false;
+
+
+  function expirer (now, exp, item, id) {
+    if (!item) {
+      exp.push(id);
+      return;
+    }
+    if (!item.ph) {
+      exp.push(id);
+      return;
+    }
+    if (now - item.created > MAXLIFETIME) {
+      exp.push(id);
+      return;
+    }
+  }
+  function remover (id) {
+    __introducees.remove(id);
+  }
+  function cleanExpired () {
+    var expired = [], _exp = expired, now = Date.now();
+    __introducees.traverse(expirer.bind(null, now, _exp));
+    _exp = null;
+    expired.forEach(remover);
+    now = null;
+    __scheduled = false;
+  }
+  function doCleaning () {
+    if (__scheduled) {
+      return;
+    }
+    __scheduled = true;
+    lib.runNext(cleanExpired);
+  }
+
   function IntroductionStrategy(){
   }
   IntroductionStrategy.prototype.destroy = lib.dummyFunc;
-  IntroductionStrategy.prototype.check = function(credentials){
-    return __introducees.remove(credentials);
+  IntroductionStrategy.prototype.check = function(credentials, bla){
+    var item = __introducees.remove(credentials);
+    doCleaning();
+    return item ? item.ph : null;
   };
   IntroductionStrategy.introduce = function(userprophash,uid){
     var _uid = uid||lib.uid();
-    __introducees.replace(_uid,userprophash);
+    __introducees.replace(_uid,{
+      created: Date.now(),
+      ph: userprophash
+    });
+    doCleaning();
     return _uid;
   };
   IntroductionStrategy.forget = function(sessionid) {
+    doCleaning();
     return __introducees.remove(sessionid);
   };
   return IntroductionStrategy;
